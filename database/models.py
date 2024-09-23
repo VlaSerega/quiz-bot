23362ac40822d35db7081a0bc894c714f2f3c6c5
VlaSerega <sergey.lipps@gmail.com>
2024-09-23 23:56:36 +0700

    Fix

--- database/models.py
+++ database/models.py
@@ -58,7 +58,7 @@ def get_sessionmaker() -> sessionmaker:
     user = getenv('POSTGRES_USER')
     password = getenv('POSTGRES_PASSWORD')
 
-    engine = create_async_engine(f'postgresql+asyncpg://user:user1234@127.0.0.1:6000/quiz-travel', echo=True)
+    engine = create_async_engine(f'postgresql+asyncpg://user:user1234@postgres:6000/quiz-travel', echo=True)
     async_session = sessionmaker(
         engine, class_=AsyncSession, expire_on_commit=False
     )
